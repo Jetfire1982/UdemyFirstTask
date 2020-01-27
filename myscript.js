9bad02e4ea2956f67f027a34edcfdf7ee6977fbf
Petr <Mclarenf1@rambler.ru>
2020-01-27 13:11:20 +0300

    second task

--- myscript.js
+++ myscript.js
@@ -1,41 +1,44 @@
 "use strict";
 
-let money=prompt("Введите Ваш бюджет на месяц", '');
-let time=prompt("Введите дату в формате YYYY-MM-DD", '');
+let money = +prompt("Введите Ваш бюджет на месяц", '');
+let time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
-let appData={
+let appData = {
   budget: money,
   timeData: time,
-  expenses:{},
-  optionalExpenses:{},
-  income:[],
-  savings:false
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  savings: false
 };
 
 
+1000
+for (let i = 0; i < 2; i++) {
+  let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    b = prompt("Во сколько обойдется?", '');
+  if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' &&
+    a.length < 50) {
+    console.log("done");
+    appData.expenses[a] = b;
+    console.log(i);
+    /*т.о. ответ пользователя об обязательной статье расходов будет являться свойством обьекта expenses, а 
+      равняться это будет значению b т.о. у нас получилась пара ключ - значение */
+  } else {
+    i--;
+  }
+}
+
+appData.moneyPerDay = appData.budget / 30; //создаем новое свойство moneyPerDay внутри нашего объекта 
+alert("Ежемесячный бюджет: " + appData.moneyPerDay);
 
 
-
-let a1=prompt("Введите обязательную статью расходов в этом месяце", ''),
-    a2=prompt("Во сколько обойдется?", '');
-
-    appData.expenses.a1=a2;
-    alert((appData.budget)/30 );
-
-
-console.log(appData.money);
-console.log(appData.time);
-
-
-
-
-
-;
-
-
-
-
-
-
-
-
+if (appData.moneyPerDay < 100) {
+  console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+  console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+  console.log("Высокий уровень достатка");
+} else {
+  console.log("Произошла ошибка");
+}
