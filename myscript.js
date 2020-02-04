97ae7e928f8cf2daa3e397f7eb3ac33588edb547
Petr <Mclarenf1@rambler.ru>
2020-02-04 10:29:51 +0300

    fourth task

--- myscript.js
+++ myscript.js
@@ -20,68 +20,83 @@ let appData = {
   expenses: {},
   optionalExpenses: {},
   income: [],
-  savings: true
-};
-
-function chooseExpenses() {
-  for (let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-      b = prompt("Во сколько обойдется?", '');
-    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' &&
-      a.length < 50) {
-      console.log("done");
-      appData.expenses[a] = b;
-      console.log(i);
-      /*т.о. ответ пользователя об обязательной статье расходов будет являться свойством обьекта expenses, а 
-        равняться это будет значению b т.о. у нас получилась пара ключ - значение */
-    } else {
-      i--;
+  savings: true,
+  chooseExpenses: function () {
+    for (let i = 0; i < 2; i++) {
+      let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
+      if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' &&
+        a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+        console.log(i);
+        /*т.о. ответ пользователя об обязательной статье расходов будет являться свойством обьекта expenses, а 
+          равняться это будет значению b т.о. у нас получилась пара ключ - значение */
+      } else {
+        i--;
+      }
     }
+  },
+  detectDayBudget: function () {
+    appData.moneyPerDay = (appData.budget / 30).toFixed(); //создаем новое свойство moneyPerDay внутри нашего объекта 
+    alert("Ежедневный бюджет: " + appData.moneyPerDay);
+  },
+  detectLevel: function () {
+    if (appData.moneyPerDay < 100) {
+      console.log("Минимальный уровень достатка");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+      console.log("Средний уровень достатка");
+    } else if (appData.moneyPerDay > 2000) {
+      console.log("Высокий уровень достатка");
+    } else {
+      console.log("Произошла ошибка");
+    }
+  },
+  //Ниже представлена функция которая поможет нам рассчитать накопления с депозита если он есть
+  checkSavings: function () {
+    if (appData.savings == true) {
+      let save = +prompt("Какова сумма накоплений?"),
+        percent = +prompt("Под какой процент?");
+      appData.monthIncome = save / 100 / 12 * percent; //создаем новое свойство monthIncome, которая будет содержать прибыль за месяц
+      alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
+    }
+  },
+  //Ниже представлена функция для определения необязательных расходов - будем заполнять объект optionalExpenses в нашей appData
+  chooseExpenses: function () {
+    for (let i = 0; i < 3; i++) {
+      appData.optionalExpenses[i] = prompt("Статья необязательных расходов?");
+    }
+  },
+  chooseIncome: function () {
+    //  
+    let items = prompt("Что принесет дополнительный доход? (Перечислите через запятую)", "");
+
+    if (typeof (items) != "string" || items == "" || typeof (items) == null) {
+      console.log("Вы ввели некорректные данные или не ввели их вовсе");
+    } else {
+      appData.income = items.split(", ");
+      appData.income.push(prompt("Может что-то еще?"));
+      appData.income.sort();
+    }
+
+    appData.income.forEach(function (itemmassive, i) {
+      alert("Способы доп. заработка: " + (i + 1) + " - " + itemmassive);
+    });
   }
 }
 
-chooseExpenses();
+appData.chooseIncome();
 
-
-function detectDayBudget() {
-  appData.moneyPerDay = (appData.budget / 30).toFixed(); //создаем новое свойство moneyPerDay внутри нашего объекта 
-  alert("Ежедневный бюджет: " + appData.moneyPerDay);
+console.log("Наша программа включает в себя данные: ")
+for (let i in appData) {
+  console.log(i);
 }
-
-detectDayBudget();
-
-
-function detectLevel() {
-  if (appData.moneyPerDay < 100) {
-    console.log("Минимальный уровень достатка");
-  } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log("Средний уровень достатка");
-  } else if (appData.moneyPerDay > 2000) {
-    console.log("Высокий уровень достатка");
-  } else {
-    console.log("Произошла ошибка");
-  }
-}
-
-detectLevel();
+console.log(Object.keys(appData));
 
 
-//Ниже представлена функция которая поможет нам рассчитать накопления с депозита если он есть
-function checkSavings() {
-  if (appData.savings == true) {
-    let save = +prompt("Какова сумма накоплений?"),
-      percent = +prompt("Под какой процент?");
-    appData.monthIncome = save / 100 / 12 * percent; //создаем новое свойство monthIncome, которая будет содержать прибыль за месяц
-    alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
-  }
-}
 
-checkSavings();
-
-
-//Ниже представлена функция для определения необязательных расходов - будем заполнять объект optionalExpenses в нашей appData
-function chooseOptExpenses() {
-  for (let i = 0; i < 3; i++) {
-    appData.optionalExpenses[i] = prompt("Статья необязательных расходов?");
-  }
-}
+// appData.chooseExpenses();
+// appData.detectDayBudget();
+// appData.detectLevel();
+// appData.checkSavings();
+// appData.chooseExpenses();
